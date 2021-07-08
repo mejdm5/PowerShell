a432184a4faf6f05afb2de38f3b2759421d1d4e0
David Mejak <dmejak@microage.com>
2021-07-08 13:27:30 -0400

    adding group_addbulkmembers_csv.ps1

---
+++ AD User and Groups/README.md
@@ -0,0 +1,1 @@
+# AD user and group manipulation scripts
---
+++ AD User and Groups/group_addbulkmembers_csv.ps1
@@ -0,0 +1,15 @@
+﻿##create CSV with list of users (samaccountname) and header of (samaccountname) to import. 
+## This foreach loop will add the users on your csv to the group specified in the targetgroup variable. 
+
+
+$users = Import-Csv "C:\exchscripts\license_samaccountnames.csv"
+
+$targetgroup = "Name of Group here"
+
+
+Foreach ($user in $users)
+{
+    Add-ADGroupMember -Identity $targetgroup -Members $user.samaccountname -Confirm:$false
+
+}
+
